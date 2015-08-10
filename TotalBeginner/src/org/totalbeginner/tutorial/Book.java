7f30c2ff33ea3b928a2fa4ef0c9c2f78daf7a16a
MajorPaine <cdpaine2003@yahoo.com>
2015-08-09 21:03:14 -0400

    Update lesson 13

--- TotalBeginner/src/org/totalbeginner/tutorial/Book.java
+++ TotalBeginner/src/org/totalbeginner/tutorial/Book.java
@@ -4,19 +4,19 @@ public class Book {
 	int test = 1;
 	String title;
 	String author;
-	
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
 	Person person;
 
 	public Book(String string) {
 		this.title = string;
 		author = "unknown author";
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
 	}
 
 	public void setPerson(Person p2) {
@@ -25,8 +25,7 @@ public class Book {
 
 	public Person getPerson() {
 		return this.person;
-		
-		
+
 	}
 
 }
